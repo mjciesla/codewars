3fc73b89d7d85aceb13ff946c14d44547f96f3dd
Mateusz Ciesla <mjciesla@protonmail.com>
2022-06-14 18:29:51 +0200

    codewars012 return count of vowels in the given string

---
+++ js/codewars012.js
@@ -0,0 +1,22 @@
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+//My solution
+function getCount(str) {
+    
+    let vowels = str.match(/[aeiou]/gi)
+     
+    return vowels === null ? 0 : vowels.length
+    
+    }
+
+//others solution
+
+    // function getCount(str) {
+    //     return str.replace(/[^aeiou]/gi, '').length;
+    //   }
+ 
