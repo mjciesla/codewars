92dfc0a8d85f48c5023cfc63d7c1718a26f64219
Mateusz Ciesla <mjciesla@protonmail.com>
2022-06-21 21:20:35 +0200

    Write a function to split a string and convert it into an array of words

---
+++ js/codewars019.js
@@ -0,0 +1,11 @@
+//Write a function to split a string and convert it into an array of words.
+//"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+
+function stringToArray(string){
+    return string.split(' ');
+  }
+
+
+  //stringToArray=(s)=>s.split(' ')
