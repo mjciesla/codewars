520b660eb7d56b343fa6b290d66f5ece036af14f
Mateusz Ciesla <mjciesla@protonmail.com>
2022-06-27 14:26:42 +0200

    remove first and last character from a string

---
+++ js/codewars026.js
@@ -0,0 +1,12 @@
+//remove first and last character from a string
+
+function removeChar(str){
+    return str.slice(1,-1)
+   }
+   
+
+   //const removeChar = str => str.slice(1,-1)
+
+//    function removeChar(str){
+//     return str.substring(1, str.length-1)
+//    }
