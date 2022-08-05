ef64578ed46cea3226a2200e3e41092ab2416499
Mateusz Ciesla <mjciesla@protonmail.com>
2022-08-05 17:43:36 +0200

    reverse string

---
+++ js/codewars066.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    return str.split('').reverse().join('')
+  }
+
+  //const solution = str => str.split('').reverse().join('');
+
+  //const solution = s => [...s].reverse().join('')
