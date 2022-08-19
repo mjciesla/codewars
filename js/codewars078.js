1ef56a04ebb32ba6aa3a6ed69294160ba6b84ccc
Mateusz Ciesla <mjciesla@protonmail.com>
2022-08-19 23:42:58 +0200

    factorial

---
+++ js/codewars078.js
@@ -0,0 +1,12 @@
+// Your task is to write function factorial.
+
+
+
+
+
+function factorial(n) {
+    let sum = 1
+    for (let i = 1; i <= n; i++)
+         sum = sum * i
+          return sum
+  }
