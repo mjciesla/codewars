3b67b995187eac584eca878c6563169666349dca
Mateusz Ciesla <mjciesla@protonmail.com>
2022-08-30 21:32:08 +0200

    mask all but last four

---
+++ js/codewars083.js
@@ -0,0 +1,27 @@
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+
+// "Skippy" --> "##ippy"
+
+// "Nananananananananananananananana Batman!"
+// -->
+// "####################################man!"
+
+
+
+
+
+
+
+
+function maskify(cc) {
+    return cc.slice(-4).padStart(cc.length, "#")
+  }
+  
